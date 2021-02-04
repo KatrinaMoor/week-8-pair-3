dc386f00c95539a2c100b14dedc5e46cf69082c2
David LeFebvre <david@lefebvre.us>
2021-02-04 12:00:54 -0500

    get transfer by id method written in TransferDAO class

--- module-2/15_Capstone/pair-exercise/java/tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDAO.java
+++ module-2/15_Capstone/pair-exercise/java/tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDAO.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.stereotype.Component;
 
 import com.techelevator.tenmo.model.Transfer;
 
+@Component
 public class TransferDAO
 {
 	@Autowired
@@ -14,6 +16,12 @@ public class TransferDAO
 	
 	public Transfer get(int id)
 	{
+		Transfer transfer = null;
+		int accountFrom;
+		int accountTo;
+		String userFrom;
+		String userTo;
+		
 		String sql = "SELECT t.transfer_id "
 					+ "		, t.account_from "
 					+ "		, t.account_to "
@@ -31,18 +39,37 @@ public class TransferDAO
 		
 		if (row.next())
 		{
-			transfer = MapRowToTransfer(row);
+			accountFrom = row.getInt("account_from");
+			accountTo = row.getInt("account_to");
+			
+			String sqlUser = "SELECT u.username " + 
+							"FROM users AS u " + 
+							"JOIN accounts AS a " + 
+							"    ON u.user_id = a.user_id " + 
+							"WHERE a.account_id = ?;";
+			
+			SqlRowSet rowUserFrom = jdbcTemplate.queryForRowSet(sqlUser, accountFrom);
+			userFrom = rowUserFrom.getString("username");
+			
+			SqlRowSet rowUserTo = jdbcTemplate.queryForRowSet(sqlUser, accountTo);
+			userTo = rowUserTo.getString("username");
+			
+			transfer = mapRowToTransfer(row, userFrom, userTo);
 		}
 		return transfer;
 	}
 	
-	private Transfer mapRowToTransfer(SqlRowSet row)
+	private Transfer mapRowToTransfer(SqlRowSet row, String userFrom, String userTo)
 	{
 		Transfer transfer = new Transfer();
 		
 		transfer.setId(row.getInt("transfer_id"));
+		transfer.setUserFrom(userFrom);
+		transfer.setUserTo(userTo);
 		transfer.setTransferType(row.getString("type"));
 		transfer.setTransferStatus(row.getString("status"));
 		transfer.setAmount(row.getBigDecimal("amount"));
+		
+		return transfer;
 	}
 }
--- module-2/15_Capstone/pair-exercise/java/tenmo-server/src/main/java/com/techelevator/tenmo/model/Transfer.java
+++ module-2/15_Capstone/pair-exercise/java/tenmo-server/src/main/java/com/techelevator/tenmo/model/Transfer.java
@@ -7,8 +7,8 @@ public class Transfer
 	private int id;
 	private String transferType;
 	private String transferStatus;
-	private User userFrom;
-	private User userTo;
+	private String userFrom;
+	private String userTo;
 	private BigDecimal amount;
 	
 	
@@ -42,22 +42,22 @@ public class Transfer
 		this.transferStatus = transferStatus;
 	}
 	
-	public User getUserFrom()
+	public String getUserFrom()
 	{
 		return userFrom;
 	}
 	
-	public void setUserFrom(User userFrom)
+	public void setUserFrom(String userFrom)
 	{
 		this.userFrom = userFrom;
 	}
 	
-	public User getUserTo()
+	public String getUserTo()
 	{
 		return userTo;
 	}
 	
-	public void setUserTo(User userTo)
+	public void setUserTo(String userTo)
 	{
 		this.userTo = userTo;
 	}
