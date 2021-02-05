cdddefc8b97eef62d2c39db1d3f720be73621158
David LeFebvre <david@lefebvre.us>
2021-02-05 14:47:25 -0500

    authentication added, error handling still missing

--- module-2/15_Capstone/pair-exercise/java/tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
+++ module-2/15_Capstone/pair-exercise/java/tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
@@ -1,6 +1,10 @@
 package com.techelevator.tenmo.controller;
 
+import java.security.Principal;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,18 +14,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.techelevator.tenmo.dao.TransferDAO;
 import com.techelevator.tenmo.dao.UserDAO;
-import com.techelevator.tenmo.dao.UserSqlDAO;
 import com.techelevator.tenmo.model.Transfer;
 
 @RestController
+@PreAuthorize("isAuthenticated()")
 @RequestMapping(path = "/transfers")
 public class TransferController
 {
 	@Autowired
-	UserSqlDAO userDao;
+	UserDAO userDao;
 	@Autowired
 	TransferDAO transferDao;
 
+	@GetMapping()
+	public List<Transfer> getCurrentUserTransfers(Principal principal)
+	{
+		int id = userDao.findIdByUsername(principal.getName());
+		List<Transfer> transfers = userDao.getTransfersByUser(id);
+		
+		return transfers;
+	}
+	
 	@GetMapping("/{id}")
 	public Transfer getById(@PathVariable int id)
 	{
@@ -29,9 +42,18 @@ public class TransferController
 	}
 	
 	@PostMapping()
-	public Transfer createTransfer(@RequestBody Transfer transfer)
+	public Transfer createTransfer(@RequestBody Transfer transfer, Principal principal)
 	{
-		return transferDao.create(transfer);
+		Transfer newTransfer = new Transfer();
+		String principalName = principal.getName();
+		
+		if(principalName.equalsIgnoreCase(transfer.getUserFrom()) ||
+				principalName.equalsIgnoreCase(transfer.getUserTo()))
+		{
+			newTransfer = transferDao.create(transfer);	
+		}
+		
+		return newTransfer;
 	}
 	
 }
